a0f0825793a62b6fd692cecae4247b3591dd2f68
victorjiang <victorjiang189@163.com>
2023-03-16 00:56:23 +0800

    feat: 增加装饰器

--- demo.js
+++ demo.js
@@ -1,72 +1,60 @@
 "use strict";
-var __decorate =
-  (this && this.__decorate) ||
-  function (decorators, target, key, desc) {
-    var c = arguments.length,
-      r =
-        c < 3
-          ? target
-          : desc === null
-          ? (desc = Object.getOwnPropertyDescriptor(target, key))
-          : desc,
-      d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function")
-      r = Reflect.decorate(decorators, target, key, desc);
-    else
-      for (var i = decorators.length - 1; i >= 0; i--)
-        if ((d = decorators[i]))
-          r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
-  };
-var __metadata =
-  (this && this.__metadata) ||
-  function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function")
-      return Reflect.metadata(k, v);
-  };
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const formatMetadataKey = Symbol("format");
+function format(formatString) {
+    return Reflect.metadata(formatMetadataKey, formatString);
+}
+function getFormat(target, propertyKey) {
+    return Reflect.getMetadata(formatMetadataKey, target, propertyKey);
+}
 let introduction = {
-  name: "victor",
-  company: "jinhai",
+    name: "victor",
+    company: "jinhai",
 };
 //定义一个类装饰器
-const freeze = (constructor) => {
-  Object.freeze(constructor);
-  Object.freeze(constructor.prototype);
+const classDecorator = (constructor) => {
+    Object.freeze(constructor);
+    Object.freeze(constructor.prototype);
 };
-//装饰器工厂返回一个方法装饰器
-function enumerable(value: boolean) {
-  return function (
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    descriptor.enumerable = value;
-  };
-}
+//定义一个方法装饰器
+const methodDecorator = (target, propertyKey, descriptor) => {
+    const method = descriptor.value;
+    method();
+    console.log('我是方法装饰器附加的功能');
+};
 let Greeter = class Greeter {
-  greeting;
-  constructor(message) {
-    this.greeting = message;
-  }
-  greet() {
-    return `Hello, ${this.greeting.name}!`;
-  }
+    greeting;
+    constructor(message) {
+        this.greeting = message;
+    }
+    greet() {
+        console.log(`Hello, ${this.greeting.name}!`);
+    }
 };
-__decorate(
-  [
-    editeIntroduction(),
+__decorate([
+    format("Hello, %s"),
+    __metadata("design:type", Object)
+], Greeter.prototype, "greeting", void 0);
+__decorate([
+    methodDecorator,
     __metadata("design:type", Function),
     __metadata("design:paramtypes", []),
-    __metadata("design:returntype", void 0),
-  ],
-  Greeter.prototype,
-  "greet",
-  null
-);
-Greeter = __decorate(
-  [freeze, __metadata("design:paramtypes", [Object])],
-  Greeter
-);
+    __metadata("design:returntype", void 0)
+], Greeter.prototype, "greet", null);
+Greeter = __decorate([
+    classDecorator,
+    __metadata("design:paramtypes", [Object])
+], Greeter);
 const greeter = new Greeter(introduction);
-// greeter.greet = () => "Hello"; //会提示 TypeError: Cannot assign to read only property 'greet' of object '#<Greeter>' 禁止修改greet对象
-console.log(greeter.greet());
+greeter.greet();
+greeter.greet = () => "Hello"; //会提示 TypeError: Cannot assign to read only property 'greet' of object '#<Greeter>' 禁止修改greet对象
--- demo.ts
+++ demo.ts
@@ -1,53 +1,97 @@
 import "reflect-metadata";
 
-const formatMetadataKey = Symbol("format");
+const formatMetadataKey = Symbol("metadataKey");
+const requiredMetadataKey = Symbol("required");
 
-function format(formatString: string) {
-  return Reflect.metadata(formatMetadataKey, formatString);
+const required = (
+  target: Object,
+  propertyKey: string | symbol,
+  parameterIndex: number
+) => {
+  let existingRequiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyKey) || [];
+  existingRequiredParameters.push(parameterIndex);
+  Reflect.defineMetadata(requiredMetadataKey, existingRequiredParameters, target, propertyKey);
 }
 
-function getFormat(target: any, propertyKey: string) {
-  return Reflect.getMetadata(formatMetadataKey, target, propertyKey);
-}
-interface Igreet {
-  name: string;
-  company: string;
-}
-
-let introduction: Igreet = {
-  name: "victor",
-  company: "jinhai",
-};
-
-//定义一个类装饰器
-const freeze: ClassDecorator = (constructor: Function) => {
-  Object.freeze(constructor);
-  Object.freeze(constructor.prototype);
-};
-//定义一个方法装饰器
-const otherFunc: MethodDecorator = (
-  target: any,
+const validate = (
+  target: Object,
   propertyKey: string | symbol,
   descriptor: PropertyDescriptor
 ) => {
-  descriptor.value = () => console.log("greet 方法被我修改了");
+  let method = descriptor.value;
+  descriptor.value = function () {
+    let requiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyKey);
+    if (requiredParameters) {
+      for (let parameterIndex of requiredParameters) {
+        if (
+          parameterIndex >= arguments.length ||
+          arguments[parameterIndex] === undefined
+        ) {
+          throw new Error("Missing required argument.");
+        }
+      }
+    }
+    return (method as Function).apply(this, arguments);
+  };
+};
+//定义一个类装饰器
+const classDecorator: ClassDecorator = (constructor: Function) => {
+  constructor.prototype.introduction = {
+    name: '高启强',
+    company: '京海建工集团',
+  };
+};
+//定义一个方法装饰器
+const methodDecorator: MethodDecorator = (
+  target: Object,
+  propertyKey: string | symbol,
+  descriptor: PropertyDescriptor
+) => {
+  //设置Greeter.greet方法不可以被修改
+  descriptor.writable = false;
 };
 
-@freeze
-class Greeter {
-  @format("Hello, %s")
-  greeting: Igreet;
+//定义一个属性装饰器工厂函数
+const propertyDescriptor = (val: string): PropertyDecorator => (
+  target: any,
+  propertyKey: string | symbol
+) => {
+  Reflect.defineMetadata(formatMetadataKey, val, target, propertyKey)
+}
 
-  constructor(message: Igreet) {
+const parameterDecorator: ParameterDecorator = (
+  target: any,
+  propertyKey: string | symbol,
+  parameterIndex: number
+) => {
+  console.log('target: ', target);
+  console.log('propertyKey: ', propertyKey);
+  console.log('parameterIndex: ', parameterIndex);
+}
+
+@classDecorator
+class Greeter {
+  greeting: string;
+  @propertyDescriptor("风浪越大鱼越贵！")
+  word?: string;
+
+  constructor(@parameterDecorator message: string = "有事找强哥") {
     this.greeting = message;
   }
 
-  // @otherFunc
+  @validate
+  getWord(@required word: string) {
+    const result = Reflect.getMetadata(formatMetadataKey, this, word);
+    console.log(result);
+  }
+
+  @methodDecorator
   greet() {
     console.log(this.greeting);
-    console.log(`Hello, ${this.greeting.name}!`);
   }
 }
-const greeter = new Greeter(introduction);
-greeter.greet();
-greeter.greet = () => "Hello"; //会提示 TypeError: Cannot assign to read only property 'greet' of object '#<Greeter>' 禁止修改greet对象
+
+const greeter = new Greeter();
+greeter.greet()
+// console.log('greeter.introduction;: ', (greeter as any).introduction); //{ name: '高启强', company: '京海建工集团', word: '风浪越大鱼越贵！' }
+// greeter.greet = () => "Hello"; //会提示 TypeError: Cannot assign to read only property 'greet' of object '#<Greeter>' 禁止修改greet对象
